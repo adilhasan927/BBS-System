c0ce127582f1808968966bf14fb8a6b843a80d23
Adil Hasan -External <adil.hasan@systemsltd.com>
2019-07-08 17:00:20 +0500

    removed help folder.

--- bbs-system-backend/routes/get-posts.js
+++ bbs-system-backend/routes/get-posts.js
@@ -1,13 +1,11 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
 
 // TODO: Add proper request handling.
 router.get('/', function(req, res, next) {
   console.log(req);
-  res.send(JSON.stringify(
-    [{author: 'author1', body: 'body1'},
-    {author: 'author2', body: 'body2'},]
-  ));
+  jsonArray = MongoClient
+  res.send(JSON.stringify());
 });
 
 module.exports = router;

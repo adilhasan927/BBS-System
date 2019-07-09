774dd6b2a10cb88136b78bfa476cc9a612b2432c
Adil Hasan -External <adil.hasan@systemsltd.com>
2019-07-09 12:30:38 +0500

    added proper token handling reminder to signup.js.

--- bbs-system-backend/routes/signup.js
+++ bbs-system-backend/routes/signup.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const connection = require('../db.js')
 
-// TODO: Add proper request handling.
+// TODO: Add proper token handling.
 router.post('/', function(req, res, next) {
   console.log(req);
   connection.then(dbs => {

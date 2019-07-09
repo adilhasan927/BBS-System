f12735a569f5628dbb044533b0102239ec92773b
Adil Hasan -External <adil.hasan@systemsltd.com>
2019-07-09 13:06:14 +0500

    added proper token handling to signup code.

--- bbs-system-backend/routes/signup.js
+++ bbs-system-backend/routes/signup.js
@@ -1,26 +1,28 @@
 var express = require('express');
 var router = express.Router();
-const connection = require('../db.js')
+const connection = require('../db.js');
 
-// TODO: Add proper token handling.
 router.post('/', function(req, res, next) {
   console.log(req);
   connection.then(dbs => {
+    token = Math.round(Math.random() * 10^4).toString();
     dbs.db('documents').collection('credentials').insertOne({
       username: req.body.username,
       password: req.body.password,
-    }).then(result => {
-      res.send(JSON.stringify({
-        authSuccessful: true,
-        token: "123456",
-      }));
-    }).catch(err => {
-      res.send(JSON.stringify({
-        authSuccessful: false,
-        token: 0,
-      }))
-    })
-  })
+      token: token,
+    });
+    return token;
+  }).then(token => {
+    res.send(JSON.stringify({
+      authSuccessful: true,
+      token: token,
+    }));
+  }).catch(err => {
+    res.send(JSON.stringify({
+      authSuccessful: false,
+      token: 0,
+    }))
+  });
 });
 
 module.exports = router;

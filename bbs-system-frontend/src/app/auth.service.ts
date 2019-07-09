1d114d23152ec89f7cbaa16e9d6df7cc78cff01a
Adil Hasan -External <adil.hasan@systemsltd.com>
2019-07-09 17:45:24 +0500

    scaffolded .post() method in auth.service.

--- bbs-system-frontend/src/app/auth.service.ts
+++ bbs-system-frontend/src/app/auth.service.ts
@@ -42,6 +42,8 @@ export class AuthService {
       );
   }
 
+  post(token, username) { }
+
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.

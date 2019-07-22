f7597b10e5dce1ae51f5f53938f8f8414c073274
Adil Hasan <adil.hasan@systemsltd.com>
2019-07-22 09:25:21 +0500

    changed interceptor order.

--- bbs-system-frontend/src/app/http-interceptors/index.ts
+++ bbs-system-frontend/src/app/http-interceptors/index.ts
@@ -5,6 +5,6 @@ import { AuthInterceptor } from './auth-interceptor';
 
 /** Http interceptor providers in outside-in order */
 export const httpInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: CachingInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: CachingInterceptor, multi: true },
 ];
